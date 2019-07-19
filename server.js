bc241fc97c0f33e3a41adf942b8000be1909d2e3
aahmed <aahmed@shapesecurity.com>
2019-07-19 10:31:38 -0700

    allow client-side routing

--- server.js
+++ server.js
@@ -4,9 +4,8 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, "build")));
 
-app.get("/", function(req, res) {
+app.get("/*", function(req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
-console.log;
 app.listen(3000, () => console.log("App listening on port 3000!"));
