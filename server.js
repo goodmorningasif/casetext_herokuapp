4d82c5bbc1989edcf7a5e7ae33fd50b293cf2c5d
aahmed <aahmed@shapesecurity.com>
2019-07-22 07:12:06 -0700

    reassign port to allow Heroku use

--- server.js
+++ server.js
@@ -8,4 +8,6 @@ app.get("/*", function(req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
-app.listen(3000, () => console.log("App listening on port 3000!"));
+app.listen(process.env.PORT || 5000, () =>
+  console.log(`App is running on port ${process.env.PORT || 5000}`)
+);
