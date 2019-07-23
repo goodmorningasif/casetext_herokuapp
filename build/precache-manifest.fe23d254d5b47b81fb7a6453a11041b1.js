dc9ab91d4d401783a7307dc9b7c62c257bbe9122
aahmed <aahmed@shapesecurity.com>
2019-07-23 11:02:09 -0700

    add build folder for heroku

---
+++ build/precache-manifest.fe23d254d5b47b81fb7a6453a11041b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1a83c8ea2d12143dd2f6a7713e7d41f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f8c8c6b9fc50552cb712",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "cbef8f5ee7fba4d0d399",
+    "url": "/static/js/2.9f4e44bf.chunk.js"
+  },
+  {
+    "revision": "f8c8c6b9fc50552cb712",
+    "url": "/static/js/main.35558889.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ae7409179d77079269d896fcaa19164f",
+    "url": "/static/media/ct-logo-dark.ae740917.webp"
+  }
+]);
